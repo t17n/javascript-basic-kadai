8fcec7ef2e4c86c2a8b19d69f9b60188197498e3
t17n <140472930+t17n@users.noreply.github.com>
2023-08-29 18:05:42 +0900

    kadai_019を提出

---
+++ kadai_019/event.js
@@ -0,0 +1,6 @@
+const change = document.getElementById('btn');
+
+change.addEventListener('click', () => {
+    const Text = document.getElementById('text');
+    Text.textContent = 'ボタンをクリックしました'
+});
